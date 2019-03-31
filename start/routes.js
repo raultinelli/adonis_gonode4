0b00b092a2bd057da12a67bc27654739661bd283
Raul Tinelli de Salles <raul.tinelli@gmail.com>
2019-03-31 19:06:07 -0300

    resete de password

--- start/routes.js
+++ start/routes.js
@@ -2,6 +2,20 @@
 
 const Route = use('Route')
 
+/**
+ * Rotas de Usuários
+ */
+
 Route.post('users', 'UserController.store')
+
+/**
+ * Rotas de sssão de usuários
+ */
+
 Route.post('sessions', 'SessionController.store')
+
+/**
+ * Rotas de troca des senha
+ */
 Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('password', 'ForgotPasswordController.update')
