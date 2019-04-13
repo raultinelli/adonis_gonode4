1939ac7fbc64f1da08edcfd09f10b68d3ce8fc46
Raul Tinelli de Salles <raul.tinelli@gmail.com>
2019-04-13 15:36:42 -0300

    models project  task

--- database/migrations/1554164352674_task_schema.js
+++ database/migrations/1554164352674_task_schema.js
@@ -5,8 +5,33 @@ const Schema = use('Schema')
 
 class TaskSchema extends Schema {
   up () {
-    this.create('tasks', (table) => {
+    this.create('tasks', table => {
       table.increments()
+      table
+        .integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        .notNullable()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+      table
+        .integer('file_id')
+        .unsigned()
+        .references('id')
+        .inTable('files')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+      table.string('title').notNullable()
+      table.timestamp('due_date')
+      table.text('description')
       table.timestamps()
     })
   }
